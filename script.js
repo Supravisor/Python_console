1edef07c487489c9cd4d03eaa4b90a3c1e436759
Supravisor <153783117+Supravisor@users.noreply.github.com>
2025-03-25 16:23:36 +1300

    remove padding from keywordArgumentBtn

--- script.js
+++ script.js
@@ -9,14 +9,11 @@ function keywordArg() {
     keywordArgumentBtn.style.color = "coral";
     keywordArgumentBtn.style.background = "white";
     keywordArgumentBtn.style.border = "2px solid coral";
-    keywordArgumentBtn.style.padding = "13px 8px";
-
   } else {
     keywordArgumentValue.value = "";
     keywordArgumentBtn.style.color = "white";
     keywordArgumentBtn.style.background = "coral";
     keywordArgumentBtn.style.border = "unset";
-    keywordArgumentBtn.style.padding = "15px 10px";
   }
 
 }
