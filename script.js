238a629b2101d9f018b741a759efd918772efd36
Supravisor <153783117+Supravisor@users.noreply.github.com>
2025-03-25 15:52:58 +1300

    create condition check for keywordArgumentValue

--- script.js
+++ script.js
@@ -4,4 +4,7 @@ const keywordArgumentBtn = document.getElementById("keyword-argument-btn");
 function keywordArg() {
   const keywordArgumentValue = document.console.keywordArgument.value;
 
+  if (keywordArgumentValue === "") {
+    }
+
 }
